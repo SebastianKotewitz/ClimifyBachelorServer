8139520344ee62519d16ae7e12894c879d666f4d
s164394 <s164394@student.dtu.dk>
2019-03-06 13:51:29 +0100

    Made validation for user and fixed validation for beacon.

--- routes/beacons.js
+++ routes/beacons.js
@@ -7,8 +7,8 @@ const _ = require('lodash');
 
 router.post('/', async (req, res) => {
 
-    //const {error} = validate(req.body);
-    //if (error) return res.status(400).send(error.details[0].message);
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
     const room = await Room.findById(req.body.roomId);
     if (!room) return res.status(404).send('Room with id ' + req.body.roomId + ' was not found');
