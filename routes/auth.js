7b6a634743b4fd96dd4480bf7d362a52422233a4
SebastianKotewitz <siggibastian@hotmail.com>
2020-04-18 23:41:05 +0200

    undid last commit, add later if needed. Old tokens would not be invalidated with the changes

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const { validateAuthorized } = require("../models/user");
 const bcrypt = require("bcryptjs");
 const { User } = require("../models/user");
 const router = express.Router();
-const { auth } = require('../middleware/auth');
 
 const FAIL_AUTH_TEXT = "Invalid email or password";
 
@@ -22,15 +21,6 @@ router.post("/", async (req, res) => {
     const token = user.generateAuthToken();
 
     res.header("x-auth-token", token).send("Login successful");
-});
-
-router.get("/renew", auth, async (req, res) => {
-    const user = await User.findById(req.user._id);
-    if (!user) return res.status(404).send("User with id " + user._id + " was not found");
-
-    const token = user.generateAuthToken();
-
-    res.header("x-auth-token", token).send("New token generated");
 });
 
 module.exports = router;
