d476718262906110b319580239a3a0d76958f297
SebastianKotewitz <siggibastian@hotmail.com>
2020-05-26 12:05:11 +0200

    added delete signal maps of room function

--- controllers/signalMapController.js
+++ controllers/signalMapController.js
@@ -92,6 +92,27 @@ const getSignalMaps = async (req, res) => {
     res.send(signalMaps);
 };
 
+const deleteSignalMapsOfRoom = async (req, res) => {
+    const roomId = req.params.roomId;
+
+    const room = await Room.findById(roomId);
+    if (!room) return res.status(404).send(`Room with id ${id} was not found in database`);
+
+    if (!req.user.adminOnBuildings.find(elem => elem.toString() === room.building.toString()))
+        return res.status(403).send("User needs to be admin on room to delete signalMaps");
+
+    const signalMaps = await SignalMap.find({room: roomId});
+    const deletedSignalMaps = [];
+
+    for (let i = 0; i < signalMaps.length; i++) {
+        await signalMaps[i].remove();
+        deletedSignalMaps.push(signalMaps[i]);
+    }
+
+    res.send(deletedSignalMaps);
+};
+
 module.exports.createSignalMap = createSignalMap;
 module.exports.confirmRoom = confirmRoom;
 module.exports.getSignalMaps = getSignalMaps;
+module.exports.deleteSignalMapsOfRoom = deleteSignalMapsOfRoom;
--- routes/signalMaps.js
+++ routes/signalMaps.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const {auth, authorized} = require("../middleware/auth");
+const validId = require("../middleware/validateIdParam");
 const router = express.Router();
-const {createSignalMap, confirmRoom, getSignalMaps} = require("../controllers/signalMapController");
+const {createSignalMap, confirmRoom, getSignalMaps, deleteSignalMapsOfRoom} = require("../controllers/signalMapController");
 
 router.post('/', [auth], createSignalMap);
 
 router.get('/', auth, getSignalMaps);
 
 router.patch("/confirm-room/:id", [auth, authorized], confirmRoom);
+
+router.delete("/:roomId", [auth, validId], deleteSignalMapsOfRoom)
 
 module.exports = router;
