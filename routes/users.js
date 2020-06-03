e84a84efe5edfe46ca46d47cf773ab353b0f4149
s174268 <46523350+s174268@users.noreply.github.com>
2020-06-03 21:13:44 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {auth, admin} = require("../middleware/auth");
-const {auth, authorized} = require("../middleware/auth");
+const {auth, admin, authorized} = require("../middleware/auth");
 const userController = require("../controllers/userController");
 
 router.post('/', userController.createUser);
