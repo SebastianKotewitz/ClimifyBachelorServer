af0fb4e7aac2f5ba7880f37596077c0e63de23dc
s174268 <46523350+s174268@users.noreply.github.com>
2020-06-03 20:59:42 +0200

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ const getUsers = async (req, res) => {
 
 const getUserIdFromEmail = async (req, res) => {
     const userid = await User.findOne({req.params.email}, "_id user id returned");
-    if (!User) return res.status(404).send(`User with email ${req.params.email} was not found`);
+    if (!userid) return res.status(404).send(`User with email ${req.params.email} was not found`);
     res.send(userid);
 };
 
